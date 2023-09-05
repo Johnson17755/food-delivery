56236ef8bdec56ebe80e1ccc03a70a81103f2944
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-09-05 14:45:27 -0700

    updating

---
+++ src/components/Layouts/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import '../../styles/Header.css';
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br />
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/Layouts/Layout';
+
+function Home() {
+  return (
+    <>
+      <Layout>
+        <div>Home</div>
+      </Layout>
+    </>
+  );
+}
+
+export default Home;
